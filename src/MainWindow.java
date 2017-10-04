04acc0df6f9a77774d718e574d0116c0ba6a1cd8
derzhylo <yderzhylo@gmail.com>
2017-10-04 11:46:41 +0300

    Initial commit

---
+++ src/MainWindow.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+/**
+ * Created by Юра on 30.09.2017.
+ */
+public class MainWindow extends JFrame {
+
+    public MainWindow(){
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320,345);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+         MainWindow mw = new MainWindow();
+    }
+}
